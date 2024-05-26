428a4436813fb5d3a3d83adeb2156d9e6f9cb1b5
Jacob <98751845+Jacob090@users.noreply.github.com>
2024-05-26 12:25:41 +0200

    Added HttpPost for Trips

---
+++ Models/Client.cs
@@ -0,0 +1,27 @@
+namespace Zadanie7.Models;
+
+public partial class Client
+{
+    public int IdClient { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string Telephone { get; set; } = null!;
+
+    public string Pesel { get; set; } = null!;
+
+    public virtual ICollection<ClientTrip> ClientTrips { get; set; } = new List<ClientTrip>();
+
+    public Client(string firstName, string lastName, string email, string telephone, string pesel)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+        Telephone = telephone;
+        Pesel = pesel;
+    }
+}
